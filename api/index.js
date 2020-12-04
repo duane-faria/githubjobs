9e89ecbc4e854e4555926d6c54e3be55ecbcdc55
duane-faria <duane.faria@etec.sp.gov.br>
2020-12-04 12:45:39 -0300

    ...

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import micro from 'micro-cors';
+
+function MyApi(req, res) {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  // handling other requests normally after this
+}
+
+const cors = micro();
+
+export default cors(MyApi);
