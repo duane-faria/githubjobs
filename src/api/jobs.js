e0efe8a0aa13f508bf411144345e0e7042e39ece
duane-faria <duane.faria@etec.sp.gov.br>
2020-12-07 12:29:31 -0300

    ...

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -18,22 +18,27 @@ export async function getJobs(params) {
     query = query.join('&');
     endpoint += `?${query}`;
   }
-  const { data } = await Api.get(endpoint);
-  // console.log(proxy);
-  fetch('https://githubjobs-lac.vercel.app/proxy', {
-    method: 'GET',
-    // mode: 'no-cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .then((d) => console.log(d));
-  // res = await res.json();
-  // console.log(res);
-
-  const gambs = await axios.get('https://githubjobs-lac.vercel.app/proxy');
-  console.log(gambs, 'gambiarra');
-  return data;
+  // const { data } = await Api.get(endpoint);
+  // // console.log(proxy);
+  // fetch('https://githubjobs-lac.vercel.app/proxy', {
+  //   method: 'GET',
+  //   // mode: 'no-cors',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json',
+  //   },
+  // })
+  //   .then((res) => res.json())
+  //   .then((d) => console.log(d));
+  // // res = await res.json();
+  // // console.log(res);
+  try {
+    const gambs = await axios.get('https://githubjobs-lac.vercel.app/proxy');
+    console.log(gambs, 'gambiarra');
+  } finally {
+    const gambs2 = await axios.get('/proxy');
+    console.log(gambs2, 'gambiarra2');
+  }
+  // return data;
+  return [];
 }
